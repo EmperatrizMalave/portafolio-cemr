f4c36066bdbc034669319a613cc7442510d7ba01
emperatrizm <emperatriz.668@gmail.com>
2025-05-02 17:12:01 -0600

    💄 ui: update layout, images and color scheme

--- src/Components/Shared/Preloader/Preloader.jsx
+++ src/Components/Shared/Preloader/Preloader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "react";
 import manimg from "../../../assets/img/banner/banner-man.png";
 const Preloader = () => {
   return (
@@ -7,7 +7,7 @@ const Preloader = () => {
         <div className="matias mb-10">
           <img src={manimg} alt="img" />
         </div>
-        <span className="fz-30 mati fw-600 text-uppercase">CEMR</span>
+        <span className="fz-30 coro fw-600 text-uppercase">CEMR</span>
       </div>
     </div>
   );
